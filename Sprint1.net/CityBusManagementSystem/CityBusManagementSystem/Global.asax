9c34a0144ba5a193af4b257700d13355acd0c3d1
MeghSar <sarkarcrj@gmail.com>
2022-02-01 12:32:35 +0530

    Updated

--- Sprint1.net/CityBusManagementSystem/CityBusManagementSystem/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CityBusManagementSystem.Global" Language="C#" %>
--- Sprint1.net/CityBusManagementSystem/CityBusManagementSystem/jquery.js
+++
@@ -1,4 +0,0 @@
-﻿$('a').click(function () {
-    $('a').removeClass('selected'); // remove selected from any other item first
-    $(this).addClass('selected'); //add selected to the one just clicked.
-});
